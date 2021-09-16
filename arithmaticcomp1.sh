f5ed3d19886a455630091d92ff0954b0f65aea9b
naveen962 <mrn28897@gmail.com>
2021-09-15 23:47:31 -0700

    first arithmatic computaion program is added

---
+++ arithmaticcomp1.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+echo "enter a"
+read a
+echo "enter b"
+read b
+echo "enter c"
+read c
+
+echo $a
+echo $b
+echo $c
+
---
+++ flipcoinsimulator4.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+head=0
+tail=0
+while :
+do
+Result=$((RANDOM%2))
+if [[  $Result == 0 ]]
+then
+	echo "Head"
+	head=$(( $head + 1 ))
+	if [[ $head == 21 ]]
+	then
+		leadingwin=$(( $head - $tail ))
+		echo "head won by:"$leadingwin
+		break
+	fi
+elif [[ $Result == 1 ]]
+then
+echo "Tail"
+tail=$(( $tail + 1 ))
+	if [[ $tail == 21 ]]
+   then
+      leadingwin=$(( $tail - $head ))
+      echo "tail won by:"$leadingwin
+      break
+	fi
+else
+echo "tie"
+fi
+done
