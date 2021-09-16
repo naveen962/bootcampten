44ac8ba0e2e2f213287293edb9c531d864352ed5
naveen962 <mrn28897@gmail.com>
2021-09-16 10:23:56 -0700

    flip2 program added

--- flip2.sh
+++ flip2.sh
@@ -2,16 +2,26 @@
 
 declare -A flip
 
+temp1=0
+temp2=0
 for (( i=1; i<=5; i++ ))
 do
 Result=$((RANDOM % 2))
 if [[  $Result == 0 ]]
 then
 	echo "Heads"
+temp1=$(($temp1 + 1))
 	flip[c$i]=H
 else
 	echo "Tails"
+temp2=$(($temp2 + 1))
 	flip[c$i]=T
 fi
 done
  echo ${flip[*]}
+echo $temp1
+echo $temp2
+HeadPercentage=$((100 * $temp1 / 5 ))
+echo "HEAD PERCENTAGE ="$HeadPercentage
+TailPercentage=$((100 * $temp2 / 5 ))
+echo "TAIL PERCENTAGE ="$TailPercentage
